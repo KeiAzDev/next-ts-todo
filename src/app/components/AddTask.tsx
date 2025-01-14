e480957ea1fc72ccc5aa0f57dab227b5e8401da6
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-13 16:54:08 -0800

    NEXTjsとtsで何かを作る始めてみました

---
+++ src/app/components/AddTask.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const AddTask = () => {
+  return (
+    <div>aaa</div>
+  )
+}
+
+export default AddTask
---
+++ src/app/components/TodoList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoList = () => {
+  return (
+    <div>
+      asdfas
+    </div>
+  )
+}
+
+export default TodoList
