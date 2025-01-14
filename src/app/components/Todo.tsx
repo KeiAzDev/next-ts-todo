571cfb09c0e4e745e712f55d699c19895dc5cfb4
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-14 08:57:19 -0800

    saveボタンが機能しない。editもできず。エラーはなし

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,19 +1,30 @@
 'use client'
+import { editTodo } from "@/api";
 import { Task } from "@/types";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface TodoProps {
   todo: Task;
 }
 
 const Todo = ({todo}: TodoProps) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   const [isEditing, setIsEditing] = useState(false);
+  const [editedTaskTitle, setEditedTaskTitle] = useState(todo.text);
+
+  useEffect(() => {
+    if(isEditing) {
+      ref.current?.focus();
+    }
+  },[isEditing])
 
   const handleEdit = async() => {
     setIsEditing(true);
   };
 
   const handleSave = async() => {
+    await editTodo(todo.id, editedTaskTitle);
     setIsEditing(false);
   };
 
@@ -23,7 +34,7 @@ const Todo = ({todo}: TodoProps) => {
       className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
     >
       {isEditing ? (
-        <input type="text" className="mr-2 py-1 px-2 rounded border-gray-400 border"/>
+        <input ref={ref} type="text" className="mr-2 py-1 px-2 rounded border-gray-400 border" value={editedTaskTitle} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEditedTaskTitle(e.target.value)}/>
       ) : (<span>{todo.text}</span>)}
       <div>
         {isEditing ? (
