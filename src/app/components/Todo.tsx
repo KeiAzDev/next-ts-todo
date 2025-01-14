05fb50a29bf4c809fd5fc5af233b39d11681974a
KeiAzDev <enopiyo3code+work@gmail.com>
2025-01-14 08:45:16 -0800

    editについてapi追加、入力先input追加、ボタン仕様変更

--- src/app/components/Todo.tsx
+++ src/app/components/Todo.tsx
@@ -1,19 +1,37 @@
+'use client'
 import { Task } from "@/types";
-import React from "react";
+import React, { useState } from "react";
 
 interface TodoProps {
   todo: Task;
 }
 
 const Todo = ({todo}: TodoProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleEdit = async() => {
+    setIsEditing(true);
+  };
+
+  const handleSave = async() => {
+    setIsEditing(false);
+  };
+
   return (
     <li
       key={todo.id}
       className="flex justify-between p-4 bg-white border-l-4 border-blue-500 rounded shadow"
     >
-      <span>{todo.text}</span>
+      {isEditing ? (
+        <input type="text" className="mr-2 py-1 px-2 rounded border-gray-400 border"/>
+      ) : (<span>{todo.text}</span>)}
       <div>
-        <button className="text-green-500 mr-3">Edit</button>
+        {isEditing ? (
+          <button className="text-blue-500 mr-3" onClick={handleSave}>Save</button>
+        ) : (
+          <button className="text-green-500 mr-3" onClick={handleEdit}>Edit</button>
+        )}
+        
         <button className="text-red-500">Delete</button>
       </div>
     </li>
